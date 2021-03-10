07dc051f84dc873e8f6a23b2639f3149e82c32ec
eldanka <eldankahrovic2208@gmail.com>
2021-03-10 20:12:37 +0100

    Adding files

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    var studlyCapVar;
+    var properCamelCae;
+    var titleCaseOver;
+
+    studlyCapVar = 10;
+    properCamelCae = "A String";
+    titleCaseOver = 9000;
+
+    return {
+        studlyCapVar,
+        properCamelCae,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+
+modul.exports = main;
